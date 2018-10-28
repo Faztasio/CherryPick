f334040658f302f327868c80887be039d2a3ae8c
Zack <zack@fbts.ca>
2018-10-27 18:17:54 -0600

    Update example1.js

--- examples/example1.js
+++ examples/example1.js
@@ -2,7 +2,11 @@ var cp = require("cherrypick");
 
 let appVersion = "1.0";
 
-cp.Router.add(/about/,function() {
-  cp.View.Create(`<div>${appVersion}</div>`);
-}
-cp.Router.listen();
+var app = cp.Component.create("Application", {
+  constructor: function() {
+    cp.Route.add(/about/,function() {
+      cp.View.create(`<div>${appVersion}</div>`)
+    })
+  }
+});
+app();
