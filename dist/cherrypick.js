db4f4ad6e6c8a68de7655f59ef10189f9630228c
Zack <zack@fbts.ca>
2018-10-27 18:10:07 -0600

    Update cherrypick.js

--- dist/cherrypick.js
+++ dist/cherrypick.js
@@ -1,5 +1,6 @@
 const lib = {
   Router: require("../lib/router.js"),
-  View: require("../lib/view.js")
+  View: require("../lib/view.js"),
+  Component: require("../lib/component.js")
 }
 module.exports = lib;
